8e5908c0f7943292018750e0ec060be0158afd2e
oranzhang <423794590@qq.com>
2012-08-01 23:15:33 +0800

    there are some error

--- forum/forum.rb
+++ forum/forum.rb
@@ -6,6 +6,7 @@ require "markdown"
 require 'mongoid'
 require 'json'
 require 'aes'
+require './MUtils'
 
 
 class Mforum < Sinatra::Base
@@ -17,116 +18,9 @@ class Mforum < Sinatra::Base
 	Mongoid.load!("#{File.dirname(__FILE__)}/config/mongoid.yml")
 	Mongoid.logger = Logger.new($stdout)
 	Moped.logger = Logger.new($stdout)
-	helpers do
-		def CreatNode(name,texts)
-			if Node.where(name:name).exists?
-				@msg = 0 #exist => 0
-			else
-				Node.create(
-					name: name,
-					texts: texts
-					)
-				@msg = 1
-			end
-			return @msg
-		end
-		def GetPostData(posthash)
-			if Post.where(hash = posthash).exists?
-				return Post.find_by(hash: posthash)
-			else
-				return 0 # 0 -> Not Exists
-			end
-		end
-		def CreatPostHash(title = "" ,texts = "",user = "" )
-			hash = Digest::SHA1.hexdigest("the post #{title}#{texts} by #{user} when #{Time.now} form #{imother}")
-			return hash
-		end
-		def PostToMongo(ititle,itexts,iuser,inode,itype,istatus,imother)
-			@hash = CreatPostHash(ititle,itexts,iuser,imother)
-			Post.create!(
-				hash: @hash,
-				title: ititle,
-				texts: itexts,
-				user: iuser,
-				time: Time.now(),
-				node: inode,
-				type: itype,
-				status: istatus, 
-				mother: imother
-			)
-		end
-		def CreatUser(iname,ipass,imail)
-			if User.where(name: iname).exists?
-				msg = 1 #
-			else if User.where(mail:imail).exists?
-				msg = 2 #Email exists => msg = 2
-			else 
-				User.create(
-					name: iname,
-					pass: ipass,
-					mail: imail,
-					regtime: Time.now(),
-					status: "user",
-					more: ""
-					) 
-				msg = 0 #Su => 0
-			end
-			return msg
-		end
-		def Login(iname,ipass)
-			if User.where(name: iname,pass:ipass).exists?
-				ck_json = '{"name" : ' + "#{iname} , " + '"mail : "' + "#{Digest::SHA1.hexdigest(User.where(name: iname,pass:ipass).mail)}"
-				cookies[:auth] = AES.encrypt(ck_json , aes_key)
-				return 1 #succ
-			else
-				return 0 #incorrent
-			end
-		end
-		def ResetPass(imail,q,a,pass)
-			if User.where(mail: imail,q: q,a: a).exists?
-				User.where(mail: imail,q: q,a: a).update(pass: pass)
-				return 0 #su
-			else
-				return 1 #err
-			end
-		end
-		def GetPostListInfo(max)
-			count_all = Post.count
-			count_topic = Post.where(type: 'topic').count
-			plus = 0
-			if count_topic%max > 0
-				plus = 1
-			end
-			page_topic = count_topic/max + plus
-			return [count_all,count_topic,page_topic].to_json
-		end
-		def GetLatestPost(max,page)
-			data = Array.new
-			for x in 1..10 do
-				num=0-(max*(page-1)+x)
-				data << Post.where(mother: 'self').sort(_id: num).limit(-1)
-			end
-			return data.to_json
-		end
-		def GetLatestPostByNode(node,max,page)
-			data = Array.new
-			for x in 1..10 do
-				num=0-(max*(page-1)+x)
-				data << Post.where(node: node,mother: 'self').sort(_id: num).limit(-1)
-			end
-			return data.to_json
-		end
-		def GetReplyByTopic(topichash)
-			data = Array.new
-			for x in 1..10 do
-				num=0-(max*(page-1)+x)
-				data << Post.where(nade: node ,mother: topichash).sort(_id: num).limit(-1)
-			end
-			return data.to_json
-		end
-	end
 
-	#helpers Sinatra::Cookies
+
+	helpers MUtils, Sinatra::Cookies
 
 
 
@@ -175,5 +69,4 @@ class Mforum < Sinatra::Base
 		""
 	end
 
-end
 end
