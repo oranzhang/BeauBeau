ef697114d3d4c4b8b81947c176ff5e18f730bd60
oran <423794590@qq.com>
2013-01-02 20:23:11 +0800

    Administration module

--- app/modules/routes.rb
+++ app/modules/routes.rb
@@ -12,3 +12,26 @@ get "/~:name" do
 		redirect 404
 	end
 end
+get "/me" do
+	@title = "我"
+	login_required
+	slim :user_settings, :layout => use_layout?
+end
+post "/pass" do
+	@title = "我"
+	login_required
+    redirect "/users" unless admin? || current_user.id.to_s == params[:id]
+    user = User.get(:id => params[:id])
+    user_attributes = params[:user]
+    if user.update(user_attributes)
+      if Rack.const_defined?('Flash')
+        flash[:notice] = '修改成功'
+      end
+      redirect '/me'
+    else
+      if Rack.const_defined?('Flash')
+        flash[:error] = "#{user.errors}"
+      end
+      redirect "/"
+    end
+end
